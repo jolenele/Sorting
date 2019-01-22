3496088b444efc00da72ac838d23c3cac2cae5f2
Nga Le <38019858+jolenele@users.noreply.github.com>
2019-01-22 11:51:27 -0500

    Add files via upload

---
+++ OrderedArray.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class OrderedArray
+    {
+        private int[] m_array;
+        private int maxSize;
+        private int numElements;
+        public OrderedArray(int size)
+        {
+            numElements = 0;
+            maxSize = size;
+            m_array = new int[size];
+        }
+        public bool addLast(int item)
+        {
+            if (numElements >= maxSize) return false;
+            m_array[numElements] = item;
+            numElements++;
+            return true;
+        }
+        public int search(int item)
+        {
+            for(int i=0; i < numElements; i++)
+            {
+                if (m_array[i] == item)
+                    return i;
+            }
+            return -1;
+        }
+        public string listItems()
+        {
+            string s = "";
+            for(int i = 0; i < numElements; i++)
+            {
+                s += m_array[i] + " ";
+            }
+            return s;
+        }
+        public void insertionSortAsc()
+        {
+            for(int i=1; i<numElements; i++)
+            {
+                int temp = m_array[i];
+                int j = i;
+                while(j>0 && m_array[j - 1] > temp)
+                {
+                    m_array[j] = m_array[j-1];
+                    j--;
+                }
+                m_array[j] = temp;
+            }
+        }
+        public void merge(int lo, int mid, int hi)
+        {
+            int i = lo;
+            int k = 0;
+            int j = mid + 1;
+            while(i<=mid && j<=hi)
+            {
+                if(m_array[i] < m_array[j])
+                {
+                    temp[k] = m_array[i];
+                    k++;
+                    j++;
+                }
+                else
+                {
+                    m_array[lo++]=m_array[mid++]
+                }
+            }
+        }
+        public void mergeSort(int lo, int hi)
+        {
+            int mid;
+            if (lo < hi)
+            {
+                mid = (lo + hi) / 2;
+                mergeSort(lo, mid);
+                mergeSort(mid+1, hi);
+                mergeSort(lo, mid, hi);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,76 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
+        static int[] arr;
+        static int triangle(int n)
+        {
+            if (n == 1) return 1;
+            return n + triangle(n - 1);
+        }
+        static int factorial(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return n * (n-1);
+        }
+        static int fibonnaci(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return fibonnaci(n - 1) + fibonnaci(n - 2);
+        }
+        static int fib(int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 1;
+            if (arr[n] != -1) return arr[n];
+            arr[n] = fib(n - 1) + fib(n - 2);
+            return arr[n];
+        }
+        static void printStars(int n)
+        {
+            if (n == 1) Console.WriteLine("*");
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                printStars(n - 1);
+            }
+        }
+        static void mergesort()
+        {
+
+        }
         static void Main(string[] args)
         {
-            UnOrderedArray ua = new UnOrderedArray(10);
-            ua.addLast(23);
-            ua.addLast(-43);
-            ua.addLast(10);
-            ua.addLast(16);
-            ua.addLast(20);
-            ua.addLast(50);
-            ua.addLast(6);
-            ua.addLast(-15);
-            ua.addLast(100);
-            ua.addLast(99);
-            ua.printList();
-            ua.andrew();
-            Console.WriteLine("\n");
-            ua.printList();
-            Console.WriteLine("\n"+ua.BinarySearch(100));
+            printStars(5);
+            Console.WriteLine("Enter an integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(triangle(n));
+            Console.WriteLine("Enter an integer: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(x));
+            Console.WriteLine("Enter an integer: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(fibonnaci(n));
+            OrderedArray arr = new OrderedArray(10);
+            arr.addLast(45);
+            arr.addLast(25);
+            arr.addLast(-7);
+            Console.WriteLine(arr.listItems());
+            arr.insertionSortAsc();
+            Console.WriteLine(arr.listItems());
+            Console.WriteLine("Enter an integer:");
+            int z = Convert.ToInt32(Console.ReadLine());
+            /*arr = new int[z+1];
+            for(int x=0; x<z+1; )*/
+
+            
+
             Console.ReadKey();
         }
     }
